a94ac6853902acb784f291d9a655d12a9285d2e3
Jon Harris <jon.harris@digital.education.gov.uk>
2023-07-31 12:03:50 +0100

    Commented out the code for CreatedAccountEventNotificationHandler because there's no registration for IUrlActionHelper which depends upon IHttpContextAccessor but that is only available in the web application not message handlers, so reverting to legacy message handling.

--- src/SFA.DAS.EmployerAccounts.MessageHandlers/EventHandlers/EmployerAccounts/CreatedAccountEvent/CreatedAccountEventNotificationHandler.cs
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers/EventHandlers/EmployerAccounts/CreatedAccountEvent/CreatedAccountEventNotificationHandler.cs
@@ -1,51 +1,51 @@
-﻿using SFA.DAS.EmployerAccounts.Commands.SendNotification;
-using SFA.DAS.EmployerAccounts.Data.Contracts;
-using SFA.DAS.EmployerAccounts.Interfaces;
-using SFA.DAS.EmployerAccounts.Messages.Events;
-using SFA.DAS.Notifications.Api.Types;
-
-namespace SFA.DAS.EmployerAccounts.MessageHandlers.EventHandlers.EmployerAccounts;
-
-public class CreatedAccountEventNotificationHandler : IHandleMessages<CreatedAccountEvent>
-{
-    private readonly ILogger<CreatedAccountEventNotificationHandler> _logger;
-    private readonly IUserAccountRepository _userRepository;
-    private readonly IMediator _mediator;
-    private readonly IUrlActionHelper _urlActionHelper;
-    private const string EmployerAccountCreatedTemplateId = "3d455ac3-876d-4bf8-8a77-07b67c06831d";
-
-    public CreatedAccountEventNotificationHandler(
-        ILogger<CreatedAccountEventNotificationHandler> logger,
-        IUserAccountRepository userRepository,
-        IMediator mediator,
-        IUrlActionHelper urlActionHelper)
-    {
-        _logger = logger;
-        _userRepository = userRepository;
-        _mediator = mediator;
-        _urlActionHelper = urlActionHelper;
-    }
-
-    public async Task Handle(CreatedAccountEvent message, IMessageHandlerContext context)
-    {
-        _logger.LogInformation($"Starting {nameof(CreatedAccountEventNotificationHandler)} handler for accountId: '{message.AccountId}'.");
-
-        var existingUser = await _userRepository.GetUserByRef(message.UserRef);
-
-        await _mediator.Send(new SendNotificationCommand
-        {
-            Email = new Email
-            {
-                RecipientsAddress = existingUser.Email,
-                TemplateId = EmployerAccountCreatedTemplateId,
-                Tokens = new Dictionary<string, string> {
-                    { "user_first_name", existingUser.FirstName },
-                    { "employer_name", message.Name },
-                    { "unsubscribe_url", _urlActionHelper.EmployerAccountsAction("/settings/notifications") }
-                }
-            }
-        });
-
-        _logger.LogInformation($"Completed {nameof(CreatedAccountEventNotificationHandler)} handler.");
-    }
-}
+﻿// using SFA.DAS.EmployerAccounts.Commands.SendNotification;
+// using SFA.DAS.EmployerAccounts.Data.Contracts;
+// using SFA.DAS.EmployerAccounts.Interfaces;
+// using SFA.DAS.EmployerAccounts.Messages.Events;
+// using SFA.DAS.Notifications.Api.Types;
+//
+// namespace SFA.DAS.EmployerAccounts.MessageHandlers.EventHandlers.EmployerAccounts;
+//
+// public class CreatedAccountEventNotificationHandler : IHandleMessages<CreatedAccountEvent>
+// {
+//     private readonly ILogger<CreatedAccountEventNotificationHandler> _logger;
+//     private readonly IUserAccountRepository _userRepository;
+//     private readonly IMediator _mediator;
+//     private readonly IUrlActionHelper _urlActionHelper;
+//     private const string EmployerAccountCreatedTemplateId = "3d455ac3-876d-4bf8-8a77-07b67c06831d";
+//
+//     public CreatedAccountEventNotificationHandler(
+//         ILogger<CreatedAccountEventNotificationHandler> logger,
+//         IUserAccountRepository userRepository,
+//         IMediator mediator,
+//         IUrlActionHelper urlActionHelper)
+//     {
+//         _logger = logger;
+//         _userRepository = userRepository;
+//         _mediator = mediator;
+//         _urlActionHelper = urlActionHelper;
+//     }
+//
+//     public async Task Handle(CreatedAccountEvent message, IMessageHandlerContext context)
+//     {
+//         _logger.LogInformation($"Starting {nameof(CreatedAccountEventNotificationHandler)} handler for accountId: '{message.AccountId}'.");
+//
+//         var existingUser = await _userRepository.GetUserByRef(message.UserRef);
+//
+//         await _mediator.Send(new SendNotificationCommand
+//         {
+//             Email = new Email
+//             {
+//                 RecipientsAddress = existingUser.Email,
+//                 TemplateId = EmployerAccountCreatedTemplateId,
+//                 Tokens = new Dictionary<string, string> {
+//                     { "user_first_name", existingUser.FirstName },
+//                     { "employer_name", message.Name },
+//                     { "unsubscribe_url", _urlActionHelper.EmployerAccountsAction("/settings/notifications") }
+//                 }
+//             }
+//         });
+//
+//         _logger.LogInformation($"Completed {nameof(CreatedAccountEventNotificationHandler)} handler.");
+//     }
+// }
