154ca0190430fca9b7daeb0c0f0e562d0c2bc38f
Jon Harris <jon.harris@digital.education.gov.uk>
2023-08-01 15:29:27 +0100

    Added logging and fixing incorrect property value for GetUserByRefQuery

--- src/SFA.DAS.EmployerAccounts/Commands/CreateAccountComplete/CreateAccountCompleteCommandHandler.cs
+++ src/SFA.DAS.EmployerAccounts/Commands/CreateAccountComplete/CreateAccountCompleteCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.EmployerAccounts.Data.Contracts;
 using SFA.DAS.EmployerAccounts.Queries.GetUserByRef;
 using SFA.DAS.Encoding;
@@ -13,26 +14,31 @@ public class CreateAccountCompleteCommandHandler : IRequestHandler<CreateAccount
     private readonly IEncodingService _encodingService;
     private readonly IMembershipRepository _membershipRepository;
     private readonly IValidator<CreateAccountCompleteCommand> _validator;
+    private readonly ILogger<CreateAccountCompleteCommandHandler> _logger;
 
     public CreateAccountCompleteCommandHandler(IEventPublisher eventPublisher, 
         IMediator mediator,
         IMembershipRepository membershipRepository,
         IEncodingService encodingService,
-        IValidator<CreateAccountCompleteCommand> validator)
+        IValidator<CreateAccountCompleteCommand> validator,
+        ILogger<CreateAccountCompleteCommandHandler> logger)
     {
         _eventPublisher = eventPublisher;
         _mediator = mediator;
         _membershipRepository = membershipRepository;
         _encodingService = encodingService;
         _validator = validator;
+        _logger = logger;
     }
 
     public async Task<Unit> Handle(CreateAccountCompleteCommand request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Starting processing of {TypeName}. Request: '{Request}'.", nameof(CreateAccountCompleteCommandHandler), request);
+        
         ValidateRequest(request);
         
         var externalUserId = Guid.Parse(request.ExternalUserId);
-        var userResponse = await _mediator.Send(new GetUserByRefQuery { UserRef = request.HashedAccountId }, cancellationToken);
+        var userResponse = await _mediator.Send(new GetUserByRefQuery { UserRef = request.ExternalUserId }, cancellationToken);
         var publicHashedAccountId = _encodingService.Encode(userResponse.User.Id, EncodingType.PublicAccountId);
 
         var caller = await _membershipRepository.GetCaller(userResponse.User.Id, request.ExternalUserId);
@@ -40,6 +46,8 @@ public class CreateAccountCompleteCommandHandler : IRequestHandler<CreateAccount
         var createdByName = caller.FullName();
 
         await PublishAccountCreatedMessage(userResponse.User.Id, request.HashedAccountId, publicHashedAccountId, request.OrganisationName, createdByName, externalUserId);
+        
+        _logger.LogInformation("Completed processing of {TypeName}.", nameof(CreateAccountCompleteCommandHandler));
 
         return Unit.Value;
     }
