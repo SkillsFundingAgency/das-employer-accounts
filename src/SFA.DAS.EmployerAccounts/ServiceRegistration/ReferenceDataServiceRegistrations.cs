f0958ee908134386e8086fbd0903399ce87d98da
Jon Harris <jon.harris@digital.education.gov.uk>
2023-09-14 08:52:31 +0100

    Adding logging to check what the ReferenceDataApi Config looks like

--- src/SFA.DAS.EmployerAccounts/ServiceRegistration/ReferenceDataServiceRegistrations.cs
+++ src/SFA.DAS.EmployerAccounts/ServiceRegistration/ReferenceDataServiceRegistrations.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using SFA.DAS.ReferenceData.Api.Client;
 
 namespace SFA.DAS.EmployerAccounts.ServiceRegistration;
@@ -8,7 +10,13 @@ public static class ReferenceDataServiceRegistrations
     public static IServiceCollection AddReferenceDataApi(this IServiceCollection services)
     {
         services.AddScoped<IReferenceDataService, ReferenceDataService>();
-        services.AddScoped<IReferenceDataApiClient>(sp => new ReferenceDataApiClient(sp.GetService<IReferenceDataApiConfiguration>()));
+        services.AddScoped<IReferenceDataApiClient>(sp =>
+        {
+            var configuration = sp.GetService<IReferenceDataApiConfiguration>();
+            var logger = sp.GetService<ILogger<ReferenceDataService>>();
+            logger.LogInformation("ReferenceDataConfiguration: {Config}", JsonConvert.SerializeObject(configuration));
+            return new ReferenceDataApiClient(configuration);
+        });
 
         return services;
     }
