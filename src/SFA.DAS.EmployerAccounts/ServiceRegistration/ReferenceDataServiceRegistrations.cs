91957783962124d2e4cea9f8101b093f83ef3e06
Jon Harris <jon.harris@digital.education.gov.uk>
2023-09-15 12:03:20 +0100

    Remove logging of config

--- src/SFA.DAS.EmployerAccounts/ServiceRegistration/ReferenceDataServiceRegistrations.cs
+++ src/SFA.DAS.EmployerAccounts/ServiceRegistration/ReferenceDataServiceRegistrations.cs
@@ -10,13 +10,7 @@ public static class ReferenceDataServiceRegistrations
     public static IServiceCollection AddReferenceDataApi(this IServiceCollection services)
     {
         services.AddScoped<IReferenceDataService, ReferenceDataService>();
-        services.AddScoped<IReferenceDataApiClient>(sp =>
-        {
-            var configuration = sp.GetService<IReferenceDataApiConfiguration>();
-            var logger = sp.GetService<ILogger<ReferenceDataService>>();
-            logger.LogInformation("ReferenceDataConfiguration: {Config}", JsonConvert.SerializeObject(configuration));
-            return new ReferenceDataApiClient(configuration);
-        });
+        services.AddScoped<IReferenceDataApiClient>(sp => new ReferenceDataApiClient(sp.GetService<IReferenceDataApiConfiguration>()));
 
         return services;
     }
