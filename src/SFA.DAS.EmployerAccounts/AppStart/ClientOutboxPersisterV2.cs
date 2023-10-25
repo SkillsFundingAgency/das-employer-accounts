e65c53ccfe691cbd7a6844fc137e1896b81c5a26
Jon Harris <jon.harris@digital.education.gov.uk>
2023-10-25 12:13:13 +0100

    Implementing "await using" for disposable objects where applicable

--- src/SFA.DAS.EmployerAccounts/AppStart/ClientOutboxPersisterV2.cs
+++ src/SFA.DAS.EmployerAccounts/AppStart/ClientOutboxPersisterV2.cs
@@ -4,7 +4,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using Newtonsoft.Json;
 using NServiceBus.Persistence;
-using NServiceBus.Persistence.Sql;
 using NServiceBus.Settings;
 using SFA.DAS.NServiceBus.Features.ClientOutbox.Data;
 using SFA.DAS.NServiceBus.Features.ClientOutbox.Models;
@@ -18,7 +17,6 @@ namespace SFA.DAS.EmployerAccounts.AppStart
     public class ClientOutboxPersisterV2 : IClientOutboxStorageV2
     {
         private readonly IDateTimeService _dateTimeService;
-
         private readonly Func<DbConnection> _connectionBuilder;
 
         public ClientOutboxPersisterV2(IDateTimeService dateTimeService, ReadOnlySettings settings)
@@ -36,17 +34,17 @@ namespace SFA.DAS.EmployerAccounts.AppStart
 
         public async Task<ClientOutboxMessageV2> GetAsync(Guid messageId, SynchronizedStorageSession synchronizedStorageSession)
         {
-            ISqlStorageSession sqlStorageSession = synchronizedStorageSession.GetSqlStorageSession();
-            using DbCommand command = sqlStorageSession.Connection.CreateCommand();
+            var sqlStorageSession = synchronizedStorageSession.GetSqlStorageSession();
+            await using var command = sqlStorageSession.Connection.CreateCommand();
             command.CommandText = "SELECT EndpointName, Operations FROM dbo.ClientOutboxData WHERE MessageId = @MessageId";
             command.CommandType = CommandType.Text;
             command.Transaction = sqlStorageSession.Transaction;
             command.AddParameter("MessageId", messageId);
-            using DbDataReader reader = await command.ExecuteReaderAsync(CommandBehavior.SingleRow).ConfigureAwait(continueOnCapturedContext: false);
+            await using var reader = await command.ExecuteReaderAsync(CommandBehavior.SingleRow).ConfigureAwait(continueOnCapturedContext: false);
             if (await reader.ReadAsync().ConfigureAwait(continueOnCapturedContext: false))
             {
-                string @string = reader.GetString(0);
-                List<TransportOperation> transportOperations = JsonConvert.DeserializeObject<List<TransportOperation>>(reader.GetString(1), new JsonSerializerSettings
+                var @string = reader.GetString(0);
+                var transportOperations = JsonConvert.DeserializeObject<List<TransportOperation>>(reader.GetString(1), new JsonSerializerSettings
                 {
                     TypeNameHandling = TypeNameHandling.Auto
                 });
@@ -58,18 +56,18 @@ namespace SFA.DAS.EmployerAccounts.AppStart
 
         public async Task<IEnumerable<IClientOutboxMessageAwaitingDispatch>> GetAwaitingDispatchAsync()
         {
-            using DbConnection connection = await _connectionBuilder.OpenConnectionAsync().ConfigureAwait(continueOnCapturedContext: false);
-            using DbCommand command = connection.CreateCommand();
+            await using var connection = await _connectionBuilder.OpenConnectionAsync().ConfigureAwait(continueOnCapturedContext: false);
+            await using var command = connection.CreateCommand();
             command.CommandText = "SELECT MessageId, EndpointName FROM dbo.ClientOutboxData WHERE Dispatched = 0 AND CreatedAt <= @CreatedAt AND PersistenceVersion = '2.0.0' ORDER BY CreatedAt";
             command.CommandType = CommandType.Text;
             command.AddParameter("CreatedAt", _dateTimeService.UtcNow.AddSeconds(-10.0));
-            using DbDataReader reader = await command.ExecuteReaderAsync().ConfigureAwait(continueOnCapturedContext: false);
-            List<IClientOutboxMessageAwaitingDispatch> clientOutboxMessages = new List<IClientOutboxMessageAwaitingDispatch>();
+            await using var reader = await command.ExecuteReaderAsync().ConfigureAwait(continueOnCapturedContext: false);
+            var clientOutboxMessages = new List<IClientOutboxMessageAwaitingDispatch>();
             while (await reader.ReadAsync().ConfigureAwait(continueOnCapturedContext: false))
             {
-                Guid guid = reader.GetGuid(0);
-                string @string = reader.GetString(1);
-                ClientOutboxMessageV2 item = new ClientOutboxMessageV2(guid, @string);
+                var guid = reader.GetGuid(0);
+                var @string = reader.GetString(1);
+                var item = new ClientOutboxMessageV2(guid, @string);
                 clientOutboxMessages.Add(item);
             }
 
@@ -78,8 +76,8 @@ namespace SFA.DAS.EmployerAccounts.AppStart
 
         public async Task SetAsDispatchedAsync(Guid messageId)
         {
-            using DbConnection connection = await _connectionBuilder.OpenConnectionAsync().ConfigureAwait(continueOnCapturedContext: false);
-            using DbCommand command = connection.CreateCommand();
+            await using var connection = await _connectionBuilder.OpenConnectionAsync().ConfigureAwait(continueOnCapturedContext: false);
+            await using var command = connection.CreateCommand();
             command.CommandText = "UPDATE dbo.ClientOutboxData SET Dispatched = 1, DispatchedAt = @DispatchedAt, Operations = '[]' WHERE MessageId = @MessageId";
             command.CommandType = CommandType.Text;
             command.AddParameter("MessageId", messageId);
@@ -89,8 +87,8 @@ namespace SFA.DAS.EmployerAccounts.AppStart
 
         public async Task SetAsDispatchedAsync(Guid messageId, SynchronizedStorageSession synchronizedStorageSession)
         {
-            ISqlStorageSession sqlStorageSession = synchronizedStorageSession.GetSqlStorageSession();
-            using DbCommand dbCommand = sqlStorageSession.Connection.CreateCommand();
+            var sqlStorageSession = synchronizedStorageSession.GetSqlStorageSession();
+            await using var dbCommand = sqlStorageSession.Connection.CreateCommand();
             dbCommand.CommandText = "UPDATE dbo.ClientOutboxData SET Dispatched = 1, DispatchedAt = @DispatchedAt, Operations = '[]' WHERE MessageId = @MessageId";
             dbCommand.CommandType = CommandType.Text;
             dbCommand.Transaction = sqlStorageSession.Transaction;
@@ -101,8 +99,8 @@ namespace SFA.DAS.EmployerAccounts.AppStart
 
         public async Task StoreAsync(ClientOutboxMessageV2 clientOutboxMessage, IClientOutboxTransaction clientOutboxTransaction)
         {
-            SqlClientOutboxTransaction sqlClientOutboxTransaction = (SqlClientOutboxTransaction)clientOutboxTransaction;
-            using DbCommand dbCommand = sqlClientOutboxTransaction.Connection.CreateCommand();
+            var sqlClientOutboxTransaction = (SqlClientOutboxTransaction)clientOutboxTransaction;
+            await using var dbCommand = sqlClientOutboxTransaction.Connection.CreateCommand();
             dbCommand.CommandText = "INSERT INTO dbo.ClientOutboxData (MessageId, EndpointName, CreatedAt, PersistenceVersion, Operations) VALUES (@MessageId, @EndpointName, @CreatedAt, '2.0.0', @Operations)";
             dbCommand.CommandType = CommandType.Text;
             dbCommand.Transaction = sqlClientOutboxTransaction.Transaction;
@@ -119,11 +117,11 @@ namespace SFA.DAS.EmployerAccounts.AppStart
 
         public async Task RemoveEntriesOlderThanAsync(DateTime oldest, CancellationToken cancellationToken)
         {
-            using DbConnection connection = await _connectionBuilder.OpenConnectionAsync().ConfigureAwait(continueOnCapturedContext: false);
-            bool flag = false;
+            await using var connection = await _connectionBuilder.OpenConnectionAsync().ConfigureAwait(continueOnCapturedContext: false);
+            var flag = false;
             while (!cancellationToken.IsCancellationRequested && !flag)
             {
-                using DbCommand command = connection.CreateCommand();
+                await using var command = connection.CreateCommand();
                 command.CommandText = "DELETE TOP(@BatchSize) FROM dbo.ClientOutboxData WHERE Dispatched = 1 AND DispatchedAt < @DispatchedBefore AND PersistenceVersion = '2.0.0'";
                 command.AddParameter("DispatchedBefore", oldest);
                 command.AddParameter("BatchSize", 10000);
