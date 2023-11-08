76ad3a6126c79c740b2fde22b3f49b765f959b64
Jon Harris <jon.harris@digital.education.gov.uk>
2023-11-08 14:05:10 +0000

    Refactoring

--- src/SFA.DAS.EmployerAccounts/Commands/UpdateUserNotificationSettings/UpdateUserNotificationSettingsCommandHandler.cs
+++ src/SFA.DAS.EmployerAccounts/Commands/UpdateUserNotificationSettings/UpdateUserNotificationSettingsCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.EmployerAccounts.Audit.Types;
 using SFA.DAS.EmployerAccounts.Commands.AuditCommand;
 using SFA.DAS.EmployerAccounts.Data.Contracts;
@@ -11,17 +12,22 @@ public class UpdateUserNotificationSettingsCommandHandler : IRequestHandler<Upda
     private readonly IAccountRepository _accountRepository;
     private readonly IValidator<UpdateUserNotificationSettingsCommand> _validator;
     private readonly IMediator _mediator;
+    private readonly ILogger<UpdateUserNotificationSettingsCommandHandler> _logger;
 
     public UpdateUserNotificationSettingsCommandHandler(IAccountRepository accountRepository,
-        IValidator<UpdateUserNotificationSettingsCommand> validator, IMediator mediator)
+        IValidator<UpdateUserNotificationSettingsCommand> validator,
+        IMediator mediator, 
+        ILogger<UpdateUserNotificationSettingsCommandHandler> logger)
     {
         _accountRepository = accountRepository;
         _validator = validator;
         _mediator = mediator;
+        _logger = logger;
     }
 
     public async Task Handle(UpdateUserNotificationSettingsCommand message, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("{TypeName} processing starting.", nameof(UpdateUserNotificationSettingsCommandHandler));
         var validationResult = _validator.Validate(message);
 
         if (!validationResult.IsValid())
@@ -29,11 +35,29 @@ public class UpdateUserNotificationSettingsCommandHandler : IRequestHandler<Upda
             throw new InvalidRequestException(validationResult.ValidationDictionary);
         }
 
-        var tasks = message.Settings.Select(setting => _mediator.Send(CreateAuditCommand(setting), cancellationToken)).ToList();
+        // var tasks = message.Settings.Select(setting => _mediator.Send(CreateAuditCommand(setting), cancellationToken)).ToList();
+        //
+        // tasks.Add(_accountRepository.UpdateUserAccountSettings(message.UserRef, message.Settings));
+        //
+        // await Task.WhenAll(tasks);
 
-        tasks.Add(_accountRepository.UpdateUserAccountSettings(message.UserRef, message.Settings));
+        _logger.LogInformation("{TypeName} Updating User Account Settings.", nameof(UpdateUserNotificationSettingsCommandHandler));
+        await _accountRepository.UpdateUserAccountSettings(message.UserRef, message.Settings);
 
-        await Task.WhenAll(tasks);
+        foreach (var setting in message.Settings)
+        {
+            _logger.LogInformation("{TypeName} Creating audit entry for setting '{Setting}'.", nameof(UpdateUserNotificationSettingsCommandHandler), setting.Name);
+            var createAuditCommand = CreateAuditCommand(setting);
+            await _mediator.Send(createAuditCommand, cancellationToken);
+        }
+
+        // var tasks = message.Settings.Select(setting => _mediator.Send(CreateAuditCommand(setting), cancellationToken)).ToList();
+        //
+        // tasks.Add(_accountRepository.UpdateUserAccountSettings(message.UserRef, message.Settings));
+        //
+        // await Task.WhenAll(tasks);
+        
+        _logger.LogInformation("{TypeName} processing completed.", nameof(UpdateUserNotificationSettingsCommandHandler));
     }
 
     private static CreateAuditCommand CreateAuditCommand(UserNotificationSetting setting)
