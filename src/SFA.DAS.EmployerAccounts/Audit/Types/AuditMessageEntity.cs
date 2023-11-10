69de6c70ce79887cd5b91ab8eca4554d153e4839
Jon Harris <jon.harris@digital.education.gov.uk>
2023-11-10 08:30:10 +0000

    Removed source* properties which are obsolete

--- src/SFA.DAS.EmployerAccounts/Audit/Types/AuditMessageEntity.cs
+++ src/SFA.DAS.EmployerAccounts/Audit/Types/AuditMessageEntity.cs
@@ -7,9 +7,6 @@ public sealed class AuditMessageEntity
     public string AffectedEntityId { get; set; }
     public string Category { get; set; }
     public string Description { get; set; }
-    public string SourceSystem { get; set; }
-    public string SourceComponent { get; set; }
-    public string SourceVersion { get; set; }
     public DateTime ChangedAt { get; set; }
     public string ChangedById { get; set; }
     public string ChangedByEmail { get; set; }
