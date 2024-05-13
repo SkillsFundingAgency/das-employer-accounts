4d3b345b06cc484b15d253fccf67a4bcd7a209eb
Jon Harris <jon.harris@digital.education.gov.uk>
2024-05-13 15:15:37 +0100

    Simplify authorisation

--- src/SFA.DAS.EmployerAccounts.Api/Controllers/EmployerTeamController.cs
+++ src/SFA.DAS.EmployerAccounts.Api/Controllers/EmployerTeamController.cs
@@ -12,11 +12,11 @@ using SFA.DAS.EmployerAccounts.Commands.ResendInvitation;
 namespace SFA.DAS.EmployerAccounts.Api.Controllers;
 
 [Route("api/team")]
+[Authorize(Policy = ApiRoles.ReadUserAccounts)]
 public class EmployerTeamController(IMediator mediator, ILogger<EmployerTeamController> logger) : ControllerBase
 {
     [HttpPut]
     [Route("change-role", Name = RouteNames.EmployerTeam.ChangeRole)]
-    [Authorize(Policy = ApiRoles.ReadUserAccounts)]
     public async Task<IActionResult> ChangeRole([FromBody] ChangeTeamMemberRoleCommand command)
     {
         try
@@ -33,7 +33,6 @@ public class EmployerTeamController(IMediator mediator, ILogger<EmployerTeamCont
 
     [HttpPost]
     [Route("resend-invitation", Name = RouteNames.EmployerTeam.ResendInvitation)]
-    [Authorize(Policy = ApiRoles.ReadUserAccounts)]
     public async Task<IActionResult> ResendInvitation([FromBody] ResendInvitationCommand command)
     {
         command.Email = WebUtility.HtmlDecode(command.Email);
