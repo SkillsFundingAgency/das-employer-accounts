415fa05f48fed9f35b1d3ca8579fd52d7958da4a
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2024-05-15 20:52:24 +0100

    Missing joins from get account legal entities

--- src/SFA.DAS.EmployerAccounts/Data/AccountLegalEntityRepository.cs
+++ src/SFA.DAS.EmployerAccounts/Data/AccountLegalEntityRepository.cs
@@ -15,7 +15,10 @@ public class AccountLegalEntityRepository :  IAccountLegalEntityRepository
 
     public async Task<List<AccountLegalEntity>> GetAccountLegalEntities(long accountId)
     {
-        var accountLegalEntities = await _db.Value.AccountLegalEntities.Where(l =>
+        var accountLegalEntities = await _db.Value.AccountLegalEntities
+            .Include(x => x.LegalEntity)
+            .Include(x => x.Agreements)
+            .Where(l =>
                  l.Account.Id == accountId &&
                  (l.PendingAgreementId != null || l.SignedAgreementId != null) &&
                  l.Deleted == null).ToListAsync();
