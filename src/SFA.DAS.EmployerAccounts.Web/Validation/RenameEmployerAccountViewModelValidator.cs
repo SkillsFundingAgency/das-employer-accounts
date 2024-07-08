54f604b3b7f80234edb7bb3fc5c88f7c890f0d76
Paul Graham <paul.graham@coprime.co.uk>
2024-07-08 16:02:51 +0100

    added ref and validator

---
+++ src/SFA.DAS.EmployerAccounts.Web/Validation/RenameEmployerAccountViewModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using SFA.DAS.InputValidation.Fluent.Extensions;
+
+namespace SFA.DAS.EmployerAccounts.Web.Validation;
+
+public sealed class RenameEmployerAccountViewModelValidator : AbstractValidator<RenameEmployerAccountViewModel>
+{
+    public RenameEmployerAccountViewModelValidator()
+    {
+        RuleFor(x => x.NewName).ValidFreeTextCharacters().WithErrorCode("NewName").WithMessage("Account name must only include letters a to z, numbers 0 to 9, and special characters such as hyphens, spaces and apostrophes");
+    }
+}
