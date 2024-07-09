054d63cf19a17f34e717f9651807505d841b28c4
Paul Graham <paul.graham@coprime.co.uk>
2024-07-09 13:17:33 +0100

    introduced Fluent assertions

--- src/SFA.DAS.EmployerAccounts.Web.UnitTests/Orchestrators/OrganisationOrchestratorTests/WhenICreateALegalEntityOfOtherType.cs
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Orchestrators/OrganisationOrchestratorTests/WhenICreateALegalEntityOfOtherType.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FluentAssertions;
 using MediatR;
 using SFA.DAS.Encoding;
 
@@ -33,8 +34,8 @@ public class WhenICreateALegalEntityOfOtherType
         var request = new OrganisationDetailsViewModel();
         var result = await _orchestrator.ValidateLegalEntityName(request);
 
-        Assert.That(result.Data.Valid, Is.False);
-        Assert.That(result.Data.ErrorDictionary.ContainsKey("Name"), Is.True);
+        result.Data.Valid.Should().BeFalse();
+        result.Data.ErrorDictionary.ContainsKey("Name").Should().BeTrue();
     }
 
     [Test]
@@ -46,7 +47,7 @@ public class WhenICreateALegalEntityOfOtherType
         };
         var result = await _orchestrator.ValidateLegalEntityName(request);
 
-        Assert.That(result.Data.Valid, Is.True);
+        result.Data.Valid.Should().BeTrue();
     }
 
     [Test]
@@ -56,7 +57,7 @@ public class WhenICreateALegalEntityOfOtherType
         request.Name = "<not Allowed>";
         var result = await _orchestrator.ValidateLegalEntityName(request);
 
-        Assert.That(result.Data.Valid, Is.False);
-        Assert.That(result.Data.ErrorDictionary.ContainsKey("Name"), Is.True);
+        result.Data.Valid.Should().BeFalse();
+        result.Data.ErrorDictionary.ContainsKey("Name").Should().BeTrue();
     }
 }
