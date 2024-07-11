8e377506af0b813ed6cdb5a581f351e3a07f5a03
Paul Graham <paul.graham@coprime.co.uk>
2024-07-11 17:11:12 +0100

    See if this validation is called

---
+++ src/SFA.DAS.EmployerAccounts.Web/Validation/RenameEmployerAccountViewModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SFA.DAS.InputValidation.Fluent.Extensions;
+
+namespace SFA.DAS.EmployerAccounts.Web.Validation;
+
+public sealed class RenameEmployerAccountViewModelValidator : AbstractValidator<RenameEmployerAccountViewModel>
+{
+    public RenameEmployerAccountViewModelValidator()
+    {
+        RuleFor(r => r.NewName).NotEmpty().WithMessage("Enter a name");
+        RuleFor(x => x.NewName).ValidFreeTextCharacters();
+    }
+}
