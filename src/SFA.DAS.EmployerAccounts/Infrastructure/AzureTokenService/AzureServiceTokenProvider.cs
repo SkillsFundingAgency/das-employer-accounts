debeb5c2a916faa0e5dde948f24f52ad455cbb23
Jon Harris <jon.harris@digital.education.gov.uk>
2024-07-19 11:10:43 +0100

    Refactor to take advantage of caching tokens by the provider

--- src/SFA.DAS.EmployerAccounts/Infrastructure/AzureTokenService/AzureServiceTokenProvider.cs
+++ src/SFA.DAS.EmployerAccounts/Infrastructure/AzureTokenService/AzureServiceTokenProvider.cs
@@ -5,17 +5,15 @@ namespace SFA.DAS.EmployerAccounts.Infrastructure.AzureTokenService;
 
 public class AzureServiceTokenProvider : IAzureServiceTokenProvider
 {
+    private readonly ChainedTokenCredential _azureServiceTokenProvider = new(
+        new ManagedIdentityCredential(),
+        new AzureCliCredential(),
+        new VisualStudioCodeCredential(),
+        new VisualStudioCredential()
+    );
+
     public async Task<string> GetTokenAsync(string resourceIdentifier)
     {
-        var azureServiceTokenProvider = new ChainedTokenCredential(
-            new ManagedIdentityCredential(),
-            new AzureCliCredential(),
-            new VisualStudioCodeCredential(),
-            new VisualStudioCredential()
-        );
-        
-        var accessToken = (await azureServiceTokenProvider.GetTokenAsync(new TokenRequestContext(scopes: new string[] { resourceIdentifier }))).Token;
-
-        return accessToken;
+        return (await _azureServiceTokenProvider.GetTokenAsync(new TokenRequestContext(scopes: [resourceIdentifier]))).Token;
     }
 }
