0f3abf56e492c7cc39a5c8f409afb398d74aad78
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-06 08:16:15 +0100

    Dealing with null hashedAccountId

--- src/SFA.DAS.EmployerAccounts.Web/Orchestrators/EmployerAccountOrchestrator.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Orchestrators/EmployerAccountOrchestrator.cs
@@ -302,7 +302,14 @@ public class EmployerAccountOrchestrator : EmployerVerificationOrchestratorBase
 
     public virtual async Task<OrchestratorResponse<AccountTaskListViewModel>> GetCreateAccountTaskList(string hashedAccountId, string userRef)
     {
-        var hasAccountId = _encodingService.TryDecode(hashedAccountId, EncodingType.AccountId, out var accountId);
+        var hasAccountId = false;
+        long accountId = 0;
+        
+        if (!string.IsNullOrEmpty(hashedAccountId))
+        {
+            hasAccountId = _encodingService.TryDecode(hashedAccountId, EncodingType.AccountId, out accountId);   
+        }
+         
         var result = await Mediator.Send(new GetCreateAccountTaskListQuery(hasAccountId ? accountId : 0, hashedAccountId, userRef));
 
         if (result == null)
