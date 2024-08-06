465ce76bb0bd8cb551f5bcbd6552456c8c5064eb
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-06 12:56:01 +0100

    Cleaning up the old code

--- src/SFA.DAS.EmployerAccounts/Infrastructure/OuterApi/Responses/Accounts/GetEmployerAccountTaskListResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace SFA.DAS.EmployerAccounts.Infrastructure.OuterApi.Responses.Accounts;
-
-public class GetEmployerAccountTaskListResponse
-    {
-        public bool HasProviders { get; set; }
-
-        public bool HasPermissions { get; set; }
-    }
