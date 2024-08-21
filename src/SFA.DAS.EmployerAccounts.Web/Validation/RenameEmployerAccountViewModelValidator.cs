e5c9ad47146beb828343c056addf7d8c3c909157
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-21 13:07:26 +0100

    Regex validation

--- src/SFA.DAS.EmployerAccounts.Web/Validation/RenameEmployerAccountViewModelValidator.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Validation/RenameEmployerAccountViewModelValidator.cs
@@ -5,6 +5,8 @@ namespace SFA.DAS.EmployerAccounts.Web.Validation;
 
 public sealed class RenameEmployerAccountViewModelValidator : AbstractValidator<RenameEmployerAccountViewModel>
 {
+    private const string ValidCharactersExpression = @"^[a-zA-Z0-9$@#()""'!,+\-=_:;.&€£*%\s\/\[\]]*$";
+    
     public RenameEmployerAccountViewModelValidator()
     {
         RuleFor(r => r.NewName).Cascade(CascadeMode.Stop)
@@ -12,9 +14,9 @@ public sealed class RenameEmployerAccountViewModelValidator : AbstractValidator<
             .WithMessage("You have entered your organisation name. If you want to use your organisation name select 'Yes, I want to use my organisation name as my employer account name'. If not, enter a new employer account name.")
             .NotEmpty()
             .WithMessage("Enter a name");
-        RuleFor(x => x.NewName)
-            // ^[a-zA-Z0-9$@#()""''!,+\-=_:;.&€£*%/[]*$
-            .Matches("^[a-zA-Z0-9$@#()\"\"''!,+\\-=_:;.&\u20ac\u00a3*%/[]*$")
+        
+    RuleFor(x => x.NewName)
+            .Matches(ValidCharactersExpression)
             .WithMessage("Account name must only include letters a to z, numbers 0 to 9, and special characters such as hyphens, spaces and apostrophes");
     }
 }
