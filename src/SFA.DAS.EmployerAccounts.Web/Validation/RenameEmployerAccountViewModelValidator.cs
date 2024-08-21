f9e8e216f496421a801855588afa805a7b011360
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-21 15:09:24 +0100

    Formatting correction

--- src/SFA.DAS.EmployerAccounts.Web/Validation/RenameEmployerAccountViewModelValidator.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Validation/RenameEmployerAccountViewModelValidator.cs
@@ -12,8 +12,8 @@ public sealed class RenameEmployerAccountViewModelValidator : AbstractValidator<
             .WithMessage("You have entered your organisation name. If you want to use your organisation name select 'Yes, I want to use my organisation name as my employer account name'. If not, enter a new employer account name.")
             .NotEmpty()
             .WithMessage("Enter a name");
-        
-    RuleFor(x => x.NewName)
+
+        RuleFor(x => x.NewName)
             .ValidFreeTextCharacters()
             .WithMessage("Account name must only include letters a to z, numbers 0 to 9, and special characters such as hyphens, spaces and apostrophes");
     }
