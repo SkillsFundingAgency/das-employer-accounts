5f3e9ba6d5efe9234f6f3a14ae9853ba6e5840f2
MarkFCain <mark.cain@digital.education.gov.uk>
2024-08-21 12:07:46 +0100

    CSP-1424 removing unneeded class

--- src/SFA.DAS.EmployerAccounts/Queries/GetPayeSchemeAccountByRef/GetPayeSchemeAccountByRefResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.EmployerAccounts.Queries.GetPayeSchemeAccountByRef;
-
-public class GetPayeSchemeAccountByRefResponse
-{
-    public long AccountId { get; set; }
-    public DateTime AddedDate { get; set; }
-    public DateTime? RemovedDate { get; set; }
-}
-
