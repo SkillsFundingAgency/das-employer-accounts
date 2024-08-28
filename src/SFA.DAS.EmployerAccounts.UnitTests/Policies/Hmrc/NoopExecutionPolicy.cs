f8de45e5fbea190855915f61e6a9fe9854cd9ba8
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-28 15:34:04 +0100

    Migrating Polly to v7

--- src/SFA.DAS.EmployerAccounts.UnitTests/Policies/Hmrc/NoopExecutionPolicy.cs
+++ src/SFA.DAS.EmployerAccounts.UnitTests/Policies/Hmrc/NoopExecutionPolicy.cs
@@ -6,22 +6,7 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Policies.Hmrc;
 
 public class NoopExecutionPolicy : ExecutionPolicy
 {
-    public override void Execute(Action action)
-    {
-    }
+    public override Task ExecuteAsync(Func<Task> action) => action();
 
-    public override Task ExecuteAsync(Func<Task> action)
-    {
-        return action();
-    }
-
-    public override T Execute<T>(Func<T> func)
-    {
-        return func();
-    }
-
-    public override Task<T> ExecuteAsync<T>(Func<Task<T>> func)
-    {
-        return func();
-    }
+    public override Task<T> ExecuteAsync<T>(Func<Task<T>> func) => func();
 }
