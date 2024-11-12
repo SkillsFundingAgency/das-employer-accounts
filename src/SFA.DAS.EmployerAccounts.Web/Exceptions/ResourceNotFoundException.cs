74963b2de6b94ae8e88ab2f9c0379c563c00e2c0
Jon Harris <jon.harris@fuitsu.com>
2024-11-12 15:49:07 +0000

    Removing obsolete exception constructors

--- src/SFA.DAS.EmployerAccounts.Web/Exceptions/ResourceNotFoundException.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Exceptions/ResourceNotFoundException.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.Serialization;
-
-namespace SFA.DAS.EmployerAccounts.Web.Exceptions;
+﻿namespace SFA.DAS.EmployerAccounts.Web.Exceptions;
 
 [Serializable]
 public class ResourceNotFoundException : Exception
@@ -8,5 +6,4 @@ public class ResourceNotFoundException : Exception
     public ResourceNotFoundException() { }
     public ResourceNotFoundException(string message) : base(message) { }
     public ResourceNotFoundException(string message, Exception inner) : base(message, inner) { }
-    protected ResourceNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 }
