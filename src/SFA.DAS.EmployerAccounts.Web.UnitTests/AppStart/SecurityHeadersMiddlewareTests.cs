88f51f6ffd7aa82c97748e4ba95525503e5db53b
ogrwill <will.ogrady@digital.education.gov.uk>
2024-11-14 12:18:16 +0000

    update csp

--- src/SFA.DAS.EmployerAccounts.Web.UnitTests/AppStart/SecurityHeadersMiddlewareTests.cs
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/AppStart/SecurityHeadersMiddlewareTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 using SFA.DAS.EmployerAccounts.Web.StartupExtensions;
 
 namespace SFA.DAS.EmployerAccounts.Web.UnitTests.AppStart;
@@ -9,10 +10,12 @@ public class SecurityHeadersMiddlewareTests
 {
     private Mock<RequestDelegate> _nextMock;
     private SecurityHeadersMiddleware _middleware;
-
+    private const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
+   
     [SetUp]
     public void Setup()
     {
+
         _nextMock = new Mock<RequestDelegate>();
         _middleware = new SecurityHeadersMiddleware(_nextMock.Object);
     }
@@ -22,14 +25,21 @@ public class SecurityHeadersMiddlewareTests
     {
         // Arrange
         var context = new DefaultHttpContext();
-
+        var cspValues = new StringValues(
+                $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} " +
+                "https://das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net https://das-pp-frnt-end.azureedge.net https://das-test-frnt-end.azureedge.net https://das-at-frnt-end.azureedge.net " +
+                "*.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com; " +
+                $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io ; " +
+                $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://*.test2-eas.apprenticeships.education.gov.uk ; " +
+                $"font-src {dasCdn} https://fonts.gstatic.com https://*.rcrsv.io data: ;" +
+                "connect-src 'self' https://*.google-analytics.com https://*.zendesk.com https://*.zdassets.com wss://*.zopim.com https://*.rcrsv.io ;");
         // Act
         await _middleware.InvokeAsync(context);
 
         // Assert
         context.Response.Headers.XFrameOptions.Should().BeEquivalentTo("DENY");
         context.Response.Headers.XContentTypeOptions.Should().BeEquivalentTo("nosniff");
-        context.Response.Headers.ContentSecurityPolicy.Should().BeEquivalentTo("default-src 'self';");
+        context.Response.Headers.ContentSecurityPolicy.Should().BeEquivalentTo(cspValues);
         context.Response.Headers.XFrameOptions.Should().BeEquivalentTo("DENY");
         context.Response.Headers["X-Permitted-Cross-Domain-Policies"].Should().BeEquivalentTo("none");
 
--- src/SFA.DAS.EmployerAccounts.Web/StartupExtensions/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.EmployerAccounts.Web/StartupExtensions/SecurityHeadersMiddleware.cs
@@ -1,12 +1,23 @@
-﻿namespace SFA.DAS.EmployerAccounts.Web.StartupExtensions;
+﻿using Microsoft.Extensions.Primitives;
+
+namespace SFA.DAS.EmployerAccounts.Web.StartupExtensions;
 
 public class SecurityHeadersMiddleware(RequestDelegate next)
 {
     public async Task InvokeAsync(HttpContext context)
     {
+        const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
+
         context.Response.Headers.Append("X-Frame-Options", "DENY");
         context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
-        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self';");
+        context.Response.Headers.Append("Content-Security-Policy", new StringValues(
+                $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} " +
+                "https://das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net https://das-pp-frnt-end.azureedge.net https://das-test-frnt-end.azureedge.net https://das-at-frnt-end.azureedge.net " +
+                "*.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com; " +
+                $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io ; " +
+                $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://*.test2-eas.apprenticeships.education.gov.uk ; " +
+                $"font-src {dasCdn} https://fonts.gstatic.com https://*.rcrsv.io data: ;" +
+                "connect-src 'self' https://*.google-analytics.com https://*.zendesk.com https://*.zdassets.com wss://*.zopim.com https://*.rcrsv.io ;"));
         context.Response.Headers.Append("X-Permitted-Cross-Domain-Policies", "none");
 
         await next(context);
