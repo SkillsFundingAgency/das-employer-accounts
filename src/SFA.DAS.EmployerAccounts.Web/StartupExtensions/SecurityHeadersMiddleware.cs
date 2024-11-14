c16951cc79e6a5b568765c92304c4dc9c43a809c
ogrwill <will.ogrady@digital.education.gov.uk>
2024-11-14 17:01:03 +0000

    simplify CSP

--- src/SFA.DAS.EmployerAccounts.Web/StartupExtensions/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.EmployerAccounts.Web/StartupExtensions/SecurityHeadersMiddleware.cs
@@ -6,19 +6,11 @@ public class SecurityHeadersMiddleware(RequestDelegate next)
 {
     public async Task InvokeAsync(HttpContext context)
     {
-        const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
-
-        context.Response.Headers.Append("X-Frame-Options", "DENY");
-        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
-        context.Response.Headers.Append("Content-Security-Policy", new StringValues(
-                $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} " +
-                "https://das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net https://das-pp-frnt-end.azureedge.net https://das-test-frnt-end.azureedge.net https://das-at-frnt-end.azureedge.net " +
-                "*.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com; " +
-                $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io ; " +
-                $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://*.test2-eas.apprenticeships.education.gov.uk ; " +
-                $"font-src {dasCdn} https://fonts.gstatic.com https://*.rcrsv.io data: ;" +
-                "connect-src 'self' https://*.google-analytics.com https://*.zendesk.com https://*.zdassets.com wss://*.zopim.com https://*.rcrsv.io ;"));
-        context.Response.Headers.Append("X-Permitted-Cross-Domain-Policies", "none");
+        context.Response.Headers.Append("x-frame-options", new StringValues("DENY"));
+        context.Response.Headers.Append("x-content-type-options", new StringValues("nosniff"));
+        context.Response.Headers.Append("X-Permitted-Cross-Domain-Policies", new StringValues("none"));
+        context.Response.Headers.Append("x-xss-protection", new StringValues("0"));
+        context.Response.Headers.Append("Content-Security-Policy", new StringValues($"default-src *; script-src *; connect-src *; img-src *; style-src *; object-src *;"));
 
         await next(context);
     }
