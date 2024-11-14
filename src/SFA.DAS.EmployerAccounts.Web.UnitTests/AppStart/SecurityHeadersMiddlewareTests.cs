a0438153dd90fdb33cdf35565f5f4f76823d5fe8
ogrwill <will.ogrady@digital.education.gov.uk>
2024-11-14 11:35:26 +0000

    add http security headers to responses

---
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/AppStart/SecurityHeadersMiddlewareTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using SFA.DAS.EmployerAccounts.Web.StartupExtensions;
+
+namespace SFA.DAS.EmployerAccounts.Web.UnitTests.AppStart;
+
+[TestFixture]
+public class SecurityHeadersMiddlewareTests
+{
+    private Mock<RequestDelegate> _nextMock;
+    private SecurityHeadersMiddleware _middleware;
+
+    [SetUp]
+    public void Setup()
+    {
+        _nextMock = new Mock<RequestDelegate>();
+        _middleware = new SecurityHeadersMiddleware(_nextMock.Object);
+    }
+
+    [Test]
+    public async Task InvokeAsync_ShouldAddSecurityHeaders()
+    {
+        // Arrange
+        var context = new DefaultHttpContext();
+
+        // Act
+        await _middleware.InvokeAsync(context);
+
+        // Assert
+        context.Response.Headers.XFrameOptions.Should().BeEquivalentTo("DENY");
+        context.Response.Headers.XContentTypeOptions.Should().BeEquivalentTo("nosniff");
+        context.Response.Headers.ContentSecurityPolicy.Should().BeEquivalentTo("default-src 'self';");
+        context.Response.Headers.XFrameOptions.Should().BeEquivalentTo("DENY");
+        context.Response.Headers["X-Permitted-Cross-Domain-Policies"].Should().BeEquivalentTo("none");
+
+        _nextMock.Verify(next => next(context), Times.Once);
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.Web/StartupExtensions/SecurityHeadersMiddleware.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.DAS.EmployerAccounts.Web.StartupExtensions;
+
+public class SecurityHeadersMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Response.Headers.Append("X-Frame-Options", "DENY");
+        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self';");
+        context.Response.Headers.Append("X-Permitted-Cross-Domain-Policies", "none");
+
+        await next(context);
+    }
+}
