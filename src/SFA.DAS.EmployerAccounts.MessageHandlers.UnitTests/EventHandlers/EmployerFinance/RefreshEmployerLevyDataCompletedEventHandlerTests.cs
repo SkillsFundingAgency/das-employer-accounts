f01a28dcdaaa21d98683e02a525dad527b046fde
ogrwill <will.ogrady@digital.education.gov.uk>
2024-11-15 10:46:36 +0000

    Reinstating sending of AccountLevyStatusCommand

---
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers.UnitTests/EventHandlers/EmployerFinance/RefreshEmployerLevyDataCompletedEventHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.EmployerAccounts.Commands.AccountLevyStatus;
+using SFA.DAS.EmployerAccounts.MessageHandlers.EventHandlers.EmployerFinance;
+using SFA.DAS.EmployerFinance.Messages.Events;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.EmployerAccounts.MessageHandlers.UnitTests.EventHandlers.EmployerFinance
+{
+    [TestFixture]
+    public class RefreshEmployerLevyDataCompletedEventHandlerTests : FluentTest<RefreshEmployerLevyDataCompletedEventHandlerTestsFixture>
+    {
+
+        [TestCase(0, ApprenticeshipEmployerType.NonLevy)]
+        [TestCase(100, ApprenticeshipEmployerType.Levy)]
+        public Task WhenMessageIsHandled_AccountLevyStatusCommandIsSent(decimal levyValue, ApprenticeshipEmployerType apprenticeshipEmployerType)
+        {
+            var timestamp = DateTime.UtcNow;
+            const long accountId = 666;
+            const bool levyImported = true;
+            const short periodMonth = 7;
+            const string periodYear = "2018";
+
+            return TestAsync(f => f.Handle(new RefreshEmployerLevyDataCompletedEvent
+            {
+                AccountId = accountId,
+                LevyImported = levyImported,
+                PeriodMonth = periodMonth,
+                PeriodYear = periodYear,
+                LevyTransactionValue = levyValue,
+                Created = timestamp
+            })
+                , (f) =>
+                {
+                    f.VerifyAccountLevyStatusCommandIsSent(accountId, apprenticeshipEmployerType);
+                });
+        }
+    }
+
+    public class RefreshEmployerLevyDataCompletedEventHandlerTestsFixture
+    {
+        private readonly RefreshEmployerLevyDataCompletedEventHandler _handler;
+        private readonly Mock<IMediator> _mediator;
+
+        public RefreshEmployerLevyDataCompletedEventHandlerTestsFixture()
+        {
+            _mediator = new Mock<IMediator>();
+
+            _handler = new RefreshEmployerLevyDataCompletedEventHandler(_mediator.Object);
+        }
+
+        public Task Handle(RefreshEmployerLevyDataCompletedEvent refreshEmployerLevyDataCompletedEvent)
+        {
+            return _handler.Handle(refreshEmployerLevyDataCompletedEvent, null);
+        }
+
+        public void VerifyAccountLevyStatusCommandIsSent(long accountId, ApprenticeshipEmployerType apprenticeshipEmployerType)
+        {
+            _mediator.Verify(e => e.Send(It.Is<AccountLevyStatusCommand>(m =>
+                m.AccountId.Equals(accountId) &&
+                m.ApprenticeshipEmployerType.Equals(apprenticeshipEmployerType)), CancellationToken.None),
+                Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers/EventHandlers/EmployerFinance/RefreshEmployerLevyDataCompletedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.EmployerAccounts.Commands.AccountLevyStatus;
+using SFA.DAS.EmployerFinance.Messages.Events;
+
+namespace SFA.DAS.EmployerAccounts.MessageHandlers.EventHandlers.EmployerFinance;
+
+public class RefreshEmployerLevyDataCompletedEventHandler(IMediator mediator) : IHandleMessages<RefreshEmployerLevyDataCompletedEvent>
+{  
+    public async Task Handle(RefreshEmployerLevyDataCompletedEvent message, IMessageHandlerContext context)
+    {
+
+        await mediator.Send(new AccountLevyStatusCommand
+        {
+            AccountId = message.AccountId,
+            ApprenticeshipEmployerType = message.LevyTransactionValue == decimal.Zero ? ApprenticeshipEmployerType.NonLevy : ApprenticeshipEmployerType.Levy
+        });
+    }
+}
