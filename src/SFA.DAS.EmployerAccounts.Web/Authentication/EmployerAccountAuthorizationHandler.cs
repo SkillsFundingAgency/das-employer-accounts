782f3dab09ee00ab64596750998effe3278f2ba7
Jon Harris <jon.harris@fuitsu.com>
2024-11-20 12:11:13 +0000

    Refactor to use AssociatedAccountsHelper

--- src/SFA.DAS.EmployerAccounts.Web/Authentication/EmployerAccountAuthorizationHandler.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Authentication/EmployerAccountAuthorizationHandler.cs
@@ -1,11 +1,9 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Newtonsoft.Json;
 using SFA.DAS.EmployerAccounts.Infrastructure;
 using SFA.DAS.EmployerAccounts.Models.UserAccounts;
-using SFA.DAS.EmployerAccounts.Services;
 using SFA.DAS.EmployerAccounts.Web.Authorization;
 using SFA.DAS.EmployerAccounts.Web.RouteValues;
 
@@ -17,103 +15,86 @@ public interface IEmployerAccountAuthorisationHandler
     Task<bool> IsOutsideAccount(AuthorizationHandlerContext context);
 }
 
-public class EmployerAccountAuthorisationHandler : IEmployerAccountAuthorisationHandler
+public class EmployerAccountAuthorisationHandler(
+    IHttpContextAccessor httpContextAccessor,
+    ILogger<EmployerAccountAuthorisationHandler> logger,
+    IAssociatedAccountsHelper associatedAccountsHelper)
+    : IEmployerAccountAuthorisationHandler
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly IUserAccountService _accountsService;
-    private readonly ILogger<EmployerAccountOwnerAuthorizationHandler> _logger;
-    private readonly EmployerAccountsConfiguration _configuration;
-
-    public EmployerAccountAuthorisationHandler(IHttpContextAccessor httpContextAccessor, IUserAccountService accountsService, ILogger<EmployerAccountOwnerAuthorizationHandler> logger, IOptions<EmployerAccountsConfiguration> configuration)
-    {
-        _httpContextAccessor = httpContextAccessor;
-        _accountsService = accountsService;
-        _logger = logger;
-        _configuration = configuration.Value;
-    }
+    // To allow unit testing
+    public int MaxPermittedNumberOfAccountsOnClaim { get; set; } = WebConstants.MaxNumberOfEmployerAccountsAllowedOnClaim;
 
     public async Task<bool> IsEmployerAuthorised(AuthorizationHandlerContext context, bool allowAllUserRoles)
     {
-        if (!_httpContextAccessor.HttpContext.Request.RouteValues.ContainsKey(RouteValueKeys.HashedAccountId))
+        if (!httpContextAccessor.HttpContext.Request.RouteValues.ContainsKey(RouteValueKeys.HashedAccountId))
         {
             return false;
         }
-        var accountIdFromUrl = _httpContextAccessor.HttpContext.Request.RouteValues[RouteValueKeys.HashedAccountId].ToString().ToUpper();
-        var employerAccountClaim = context.User.FindFirst(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier));
-
-        if (employerAccountClaim?.Value == null)
-            return false;
 
         Dictionary<string, EmployerUserAccountItem> employerAccounts;
 
         try
         {
-            employerAccounts = JsonConvert.DeserializeObject<Dictionary<string, EmployerUserAccountItem>>(employerAccountClaim.Value);
+            employerAccounts = await associatedAccountsHelper.GetAssociatedAccounts(forceRefresh: false);
         }
-        catch (JsonSerializationException e)
+        catch (Exception e)
         {
-            _logger.LogError(e, "Could not deserialize employer account claim for user");
+            logger.LogError(e, "Unable to retrieve employer account claim for user");
             return false;
         }
-        
+
         EmployerUserAccountItem employerIdentifier = null;
+
+        var accountIdFromUrl = httpContextAccessor.HttpContext.Request.RouteValues[RouteValueKeys.HashedAccountId].ToString().ToUpper();
 
         if (employerAccounts != null)
         {
             employerIdentifier = employerAccounts.ContainsKey(accountIdFromUrl)
-                ? employerAccounts[accountIdFromUrl] : null;
+                ? employerAccounts[accountIdFromUrl]
+                : null;
         }
 
-        if (employerAccounts == null || !employerAccounts.ContainsKey(accountIdFromUrl))
+        if (!employerAccounts.ContainsKey(accountIdFromUrl))
         {
             if (!context.User.HasClaim(c => c.Type.Equals(ClaimTypes.NameIdentifier)))
+            {
                 return false;
+            }
 
-            var userClaim = context.User.Claims
-                .First(c => c.Type.Equals(ClaimTypes.NameIdentifier));
+            var result = await associatedAccountsHelper.GetAssociatedAccounts(forceRefresh: true);
 
-            var email = context.User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.Email))?.Value;
-
-            var userId = userClaim.Value;
-
-            var result = await _accountsService.GetUserAccounts(userId, email);
-
-            var accountsAsJson = JsonConvert.SerializeObject(result.EmployerAccounts.ToDictionary(k => k.AccountId));
+            var accountsAsJson = JsonConvert.SerializeObject(result);
             var associatedAccountsClaim = new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, accountsAsJson, JsonClaimValueTypes.Json);
 
             var updatedEmployerAccounts = JsonConvert.DeserializeObject<Dictionary<string, EmployerUserAccountItem>>(associatedAccountsClaim.Value);
-
-            userClaim.Subject.AddClaim(associatedAccountsClaim);
 
             if (!updatedEmployerAccounts.ContainsKey(accountIdFromUrl))
             {
                 return false;
             }
+
             employerIdentifier = updatedEmployerAccounts[accountIdFromUrl];
         }
 
-        if (!_httpContextAccessor.HttpContext.Items.ContainsKey(ContextItemKeys.EmployerIdentifier))
+        if (!httpContextAccessor.HttpContext.Items.ContainsKey(ContextItemKeys.EmployerIdentifier))
         {
-            _httpContextAccessor.HttpContext.Items.Add(ContextItemKeys.EmployerIdentifier, employerAccounts.GetValueOrDefault(accountIdFromUrl));
+            httpContextAccessor.HttpContext.Items.Add(ContextItemKeys.EmployerIdentifier, employerAccounts.GetValueOrDefault(accountIdFromUrl));
         }
 
-        if (!CheckUserRoleForAccess(employerIdentifier, allowAllUserRoles))
-        {
-            return false;
-        }
-
-        return true;
+        return CheckUserRoleForAccess(employerIdentifier, allowAllUserRoles);
     }
 
     public Task<bool> IsOutsideAccount(AuthorizationHandlerContext context)
     {
-        if (_httpContextAccessor.HttpContext.Request.RouteValues.ContainsKey(RouteValueKeys.HashedAccountId))
+        if (httpContextAccessor.HttpContext.Request.RouteValues.ContainsKey(RouteValueKeys.HashedAccountId))
         {
             return Task.FromResult(false);
         }
 
         if (!context.User.HasClaim(c => c.Type.Equals(ClaimTypes.NameIdentifier)))
+        {
             return Task.FromResult(false);
+        }
 
         return Task.FromResult(true);
     }
