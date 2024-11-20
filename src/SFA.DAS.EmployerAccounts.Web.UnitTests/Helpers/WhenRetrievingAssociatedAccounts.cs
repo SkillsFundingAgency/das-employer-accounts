6c4ebb0e2e6c480d1b43ce1bb1a372fbd56481f6
Jon Harris <jon.harris@fuitsu.com>
2024-11-20 12:10:49 +0000

    Adding helper to retrieve associated accounts

---
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Helpers/WhenRetrievingAssociatedAccounts.cs
@@ -0,0 +1,186 @@
+using System.Security.Claims;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SFA.DAS.EmployerAccounts.Infrastructure;
+using SFA.DAS.EmployerAccounts.Models.UserAccounts;
+using SFA.DAS.EmployerAccounts.Services;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerAccounts.Web.UnitTests.Helpers;
+
+public class WhenRetrievingAssociatedAccounts
+{
+    [Test, MoqAutoData]
+    public async Task Then_User_EmployerAccounts_Should_Be_Retrieved_From_Claims_When_Populated_And_ForceRefresh_Is_False(
+        string userId,
+        string email,
+        [Frozen] Mock<IHttpContextAccessor> httpContextAccessor,
+        Mock<ILogger<AssociatedAccountsHelper>> logger,
+        Mock<IUserAccountService> userAccountService,
+        Dictionary<string, EmployerUserAccountItem> accountData
+    )
+    {
+        //Arrange
+        var serialisedAccounts = JsonConvert.SerializeObject(accountData);
+
+        var claimsPrinciple = new ClaimsPrincipal([
+            new ClaimsIdentity([
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.NameIdentifier, userId),
+                new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, serialisedAccounts),
+            ])
+        ]);
+
+        var httpContext = new DefaultHttpContext(new FeatureCollection())
+        {
+            User = claimsPrinciple
+        };
+
+        httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext);
+
+        var helper = new AssociatedAccountsHelper(userAccountService.Object, httpContextAccessor.Object, logger.Object)
+        {
+            MaxPermittedNumberOfAccountsOnClaim = accountData.Count
+        };
+
+        //Act
+        await helper.GetAssociatedAccounts(forceRefresh: false);
+
+        //Assert
+        userAccountService.Verify(x => x.GetUserAccounts(userId, email), Times.Never);
+        claimsPrinciple.Claims.Should().Contain(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier));
+
+        var actualClaimValue = claimsPrinciple.Claims.First(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier)).Value;
+        actualClaimValue.Should().Be(serialisedAccounts);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Then_User_EmployerAccounts_Should_Be_Retrieved_From_UserService_When_Claims_Are_Populated_And_ForceRefresh_Is_True(
+        string userId,
+        string email,
+        [Frozen] Mock<IHttpContextAccessor> httpContextAccessor,
+        Mock<ILogger<AssociatedAccountsHelper>> logger,
+        Mock<IUserAccountService> userAccountService,
+        Dictionary<string, EmployerUserAccountItem> existingAccountData,
+        EmployerUserAccounts updatedAccountData
+    )
+    {
+        //Arrange
+        var claimsPrinciple = new ClaimsPrincipal([
+            new ClaimsIdentity([
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.NameIdentifier, userId),
+                new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, JsonConvert.SerializeObject(existingAccountData)),
+            ])
+        ]);
+
+        var httpContext = new DefaultHttpContext(new FeatureCollection())
+        {
+            User = claimsPrinciple
+        };
+
+        httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext);
+         userAccountService.Setup(x => x.GetUserAccounts(userId, email)).ReturnsAsync(updatedAccountData);
+
+        var helper = new AssociatedAccountsHelper(userAccountService.Object, httpContextAccessor.Object, logger.Object)
+        {
+            MaxPermittedNumberOfAccountsOnClaim = existingAccountData.Count
+        };
+
+        //Act
+        await helper.GetAssociatedAccounts(forceRefresh: true);
+
+        //Assert
+        userAccountService.Verify(x => x.GetUserAccounts(userId, email), Times.Once);
+        claimsPrinciple.Claims.Should().Contain(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier));
+
+        var actualClaimValue = claimsPrinciple.Claims.First(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier)).Value;
+        var expectedClaimValue = JsonConvert.SerializeObject(updatedAccountData.EmployerAccounts.ToDictionary(x => x.AccountId));
+        actualClaimValue.Should().Be(expectedClaimValue);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Then_User_EmployerAccounts_Should_Be_Retrieved_From_AccountsService_And_Stored_When_Claim_Value_Is_Empty_And_Within_Max_Number_Of_Accounts(
+        string userId,
+        string email,
+        [Frozen] Mock<IHttpContextAccessor> httpContextAccessor,
+        Mock<ILogger<AssociatedAccountsHelper>> logger,
+        Mock<IUserAccountService> userAccountService,
+        EmployerUserAccounts accountData
+    )
+    {
+        //Arrange
+        var claimsPrinciple = new ClaimsPrincipal([
+            new ClaimsIdentity([
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.NameIdentifier, userId)
+            ])
+        ]);
+
+        var httpContext = new DefaultHttpContext(new FeatureCollection())
+        {
+            User = claimsPrinciple
+        };
+
+        httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext);
+        userAccountService.Setup(x => x.GetUserAccounts(userId, email)).ReturnsAsync(accountData);
+
+        var helper = new AssociatedAccountsHelper(userAccountService.Object, httpContextAccessor.Object, logger.Object)
+        {
+            MaxPermittedNumberOfAccountsOnClaim = accountData.EmployerAccounts.Count()
+        };
+
+        //Act
+        await helper.GetAssociatedAccounts(forceRefresh: false);
+
+        //Assert
+        userAccountService.Verify(x => x.GetUserAccounts(userId, email), Times.Once);
+        claimsPrinciple.Claims.Should().Contain(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier));
+
+        var actualClaimValue = claimsPrinciple.Claims.First(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier)).Value;
+        JsonConvert.SerializeObject(accountData.EmployerAccounts.ToDictionary(k => k.AccountId)).Should().Be(actualClaimValue);
+    }
+    
+    [Test, MoqAutoData]
+    public async Task Then_User_EmployerAccounts_Should_Be_Retrieved_From_AccountsService_And_Not_Stored_When_Claim_Value_Is_Empty_And_Above_Max_Number_Of_Accounts(
+        string userId,
+        string email,
+        [Frozen] Mock<IHttpContextAccessor> httpContextAccessor,
+        Mock<ILogger<AssociatedAccountsHelper>> logger,
+        Mock<IUserAccountService> userAccountService,
+        EmployerUserAccounts accountData
+    )
+    {
+        //Arrange
+        var claimsPrinciple = new ClaimsPrincipal([
+            new ClaimsIdentity([
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.NameIdentifier, userId)
+            ])
+        ]);
+
+        var httpContext = new DefaultHttpContext(new FeatureCollection())
+        {
+            User = claimsPrinciple
+        };
+
+        httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext);
+        userAccountService.Setup(x => x.GetUserAccounts(userId, email)).ReturnsAsync(accountData);
+
+        var helper = new AssociatedAccountsHelper(userAccountService.Object, httpContextAccessor.Object, logger.Object)
+        {
+            MaxPermittedNumberOfAccountsOnClaim = accountData.EmployerAccounts.Count() - 1
+        };
+
+        //Act
+        await helper.GetAssociatedAccounts(forceRefresh: false);
+
+        //Assert
+        userAccountService.Verify(x => x.GetUserAccounts(userId, email), Times.Once);
+        claimsPrinciple.Claims.Should().NotContain(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier));
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.Web/Helpers/AssociatedAccountsHelper.cs
@@ -0,0 +1,62 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Newtonsoft.Json;
+using SFA.DAS.EmployerAccounts.Infrastructure;
+using SFA.DAS.EmployerAccounts.Models.UserAccounts;
+using SFA.DAS.EmployerAccounts.Services;
+
+namespace SFA.DAS.EmployerAccounts.Web.Helpers;
+
+public interface IAssociatedAccountsHelper
+{
+    Task<Dictionary<string, EmployerUserAccountItem>> GetAssociatedAccounts(bool forceRefresh);
+}
+
+public class AssociatedAccountsHelper(IUserAccountService accountsService, IHttpContextAccessor httpContextAccessor, ILogger<AssociatedAccountsHelper> logger) : IAssociatedAccountsHelper
+{
+    // To allow unit testing
+    public int MaxPermittedNumberOfAccountsOnClaim { get; set; } = WebConstants.MaxNumberOfEmployerAccountsAllowedOnClaim;
+
+    public async Task<Dictionary<string, EmployerUserAccountItem>> GetAssociatedAccounts(bool forceRefresh)
+    {
+        var user = httpContextAccessor.HttpContext.User;
+        var employerAccountsClaim = user.FindFirst(c => c.Type.Equals(EmployerClaims.AccountsClaimsTypeIdentifier));
+
+        if (!forceRefresh && employerAccountsClaim != null)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, EmployerUserAccountItem>>(employerAccountsClaim.Value);
+            }
+            catch (JsonSerializationException e)
+            {
+                logger.LogError(e, "Could not deserialize employer account claim for user");
+                throw;
+            }
+        }
+
+        var userClaim = user.Claims.First(c => c.Type.Equals(ClaimTypes.NameIdentifier));
+
+        var email = user.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.Email))?.Value;
+        var userId = userClaim.Value;
+
+        var result = await accountsService.GetUserAccounts(userId, email);
+        var associatedAccounts = result.EmployerAccounts.ToDictionary(k => k.AccountId);
+
+        // Some users have 100's of employer accounts. The claims cannot handle that volume of data.
+        if (associatedAccounts.Count <= MaxPermittedNumberOfAccountsOnClaim)
+        {
+            var accountsAsJson = JsonConvert.SerializeObject(associatedAccounts);
+            var associatedAccountsClaim = new Claim(EmployerClaims.AccountsClaimsTypeIdentifier, accountsAsJson, JsonClaimValueTypes.Json);
+
+            if (employerAccountsClaim != null)
+            {
+                userClaim.Subject!.RemoveClaim(employerAccountsClaim);
+            }
+
+            userClaim.Subject!.AddClaim(associatedAccountsClaim);
+        }
+
+        return associatedAccounts;
+    }
+}
