cd424e53bef48e8f3a414cc8f85b0a0d6793ca86
ogrwill <will.ogrady@digital.education.gov.uk>
2024-11-27 08:44:59 +0000

    tidy

--- src/SFA.DAS.EmployerAccounts.Web.UnitTests/AppStart/SecurityHeadersMiddlewareTests.cs
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/AppStart/SecurityHeadersMiddlewareTests.cs
@@ -10,7 +10,7 @@ public class SecurityHeadersMiddlewareTests
 {
     private Mock<RequestDelegate> _nextMock;
     private SecurityHeadersMiddleware _middleware;
-    const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net";
+    private const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net";
 
     [SetUp]
     public void Setup()
